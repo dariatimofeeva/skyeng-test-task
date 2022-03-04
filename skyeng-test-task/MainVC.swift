d41cb2283a616262d4c2a18052c15ee63e78330e
dariatimofeeva <79119014846@yandex.ru>
2022-03-04 17:00:30 +0300

    Added file tableViewCell, IBOutlet and IBAction, func tableView

--- skyeng-test-task/MainVC.swift
+++ skyeng-test-task/MainVC.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    
+    @IBOutlet weak var wordSearchTF: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    struct WordData {
+        var meaning: [String : Any] = ["id" : "string",
+                        "wordID" : 0,
+                       "text" : "string"]
+        var images = ["url" : "string"]
+        var definition = ["deffinition" : "string"]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+    
+    @IBAction func findWordBtn(_ sender: UIButton) {
+    }
+    
 }
 
---
+++ skyeng-test-task/tableViewCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  tableViewCellTableViewCell.swift
+//  skyeng-test-task
+//
+//  Created by Дарья Тимофеева on 04.03.2022.
+//
+
+import UIKit
+
+class tableViewCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
